8c71dd7c1eaf6620416f98a1cbbe4a9e9ffb33cb
Varunkumar8055 <149817216+Varunkumar8055@users.noreply.github.com>
2025-03-18 14:41:44 +0530

    implement CRUD operations for contacts API and set up Prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { prismaClient } from "./prismaa";
 
-const app = new Hono()
+const hono = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+hono.get("/api/contacts", async (context) => {
+  const contacts = await prismaClient.contact.findMany();
+  return context.json({ contacts }, 200);
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+hono.post("/api/contacts", async (context) => {
+  const { name, phone, email } = await context.req.json();
+  const contact = await prismaClient.contact.create({
+    data: { name, phone, email },
+  });
+  return context.json({ contact }, 201);
+});
+
+hono.patch("/api/contacts/:id", async (context) => {
+  const { id } = context.req.param();
+  const { name, phone, email } = await context.req.json();
+  const contact = await prismaClient.contact.update({
+    where: { id },
+    data: { name, phone, email },
+  });
+  return context.json({ contact }, 200);
+});
+
+hono.delete("/api/contacts/:id", async (context) => {
+  const { id } = context.req.param();
+  const existingContact = await prismaClient.contact.findUnique({
+    where: { id },
+  });
+  if (existingContact) {
+    await prismaClient.contact.delete({ where: { id } });
+    return context.json({ contact: existingContact }, 200);
+  } else {
+    return context.notFound();
+  }
+});
+
+serve(hono, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`);
+});
